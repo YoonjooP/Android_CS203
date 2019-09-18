0d9eaa774a9f3c2611db6152aef217f2766448b8
YoonjooP <dbswn824@go.byuh.edu>
2019-09-17 16:23:13 -1000

    test

--- app/src/main/java/edu/byuh/cis/cs203/hellocs203/GameView.java
+++ app/src/main/java/edu/byuh/cis/cs203/hellocs203/GameView.java
@@ -41,11 +41,39 @@ public class GameView extends View {
                 float shipsize = w * 0.2f;
                 battleship = Bitmap.createScaledBitmap(battleship,
                         (int) shipsize, (int) shipsize, true);
+                float bigAirsize = w * 0.2f;
+                bigAir = Bitmap.createScaledBitmap(bigAir,
+                        (int) bigAirsize, (int) bigAirsize, true);
+                float bigSubsize = w * 0.2f;
+                bigSub = Bitmap.createScaledBitmap(bigSub,
+                        (int) bigSubsize, (int) bigSubsize, true);
+                float litAirsize = w * 0.1f;
+                litAir = Bitmap.createScaledBitmap(litAir,
+                        (int) litAirsize, (int) litAirsize, true);
+                float litSubsize = w * 0.1f;
+                litSub = Bitmap.createScaledBitmap(litSub,
+                        (int) litSubsize, (int) litSubsize, true);
+                float medAirsize = w * 0.15f;
+                medAir = Bitmap.createScaledBitmap(medAir,
+                        (int) medAirsize, (int) medAirsize, true);
+                float medSubsize = w * 0.15f;
+                medSub = Bitmap.createScaledBitmap(medSub,
+                        (int) medSubsize, (int) medSubsize, true);
+                float watersize = w * 0.05f;
+                water = Bitmap.createScaledBitmap(water,
+                        (int) watersize, (int) watersize, true);
+
                 init = true;
             }
             c.drawColor(Color.GREEN);
             c.drawBitmap(battleship, 0, 0, null);
-            //somechanges123
+            c.drawBitmap(bigAir, 0, 0, null);
+            c.drawBitmap(bigSub, 0, 0, null);
+            c.drawBitmap(litAir, 0, 0, null);
+            c.drawBitmap(litSub, 0, 0, null);
+            c.drawBitmap(medAir, 0, 0, null);
+            c.drawBitmap(medSub, 0, 0, null);
+            c.drawBitmap(water, 0, 0, null);
         }
 
 }
