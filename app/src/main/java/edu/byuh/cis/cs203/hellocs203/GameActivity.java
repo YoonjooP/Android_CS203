cc27ee9dc8bc34f39efbcbf7e374a971b5f50555
YoonjooP <dbswn824@go.byuh.edu>
2019-09-17 15:09:17 -1000

    test

---
+++ app/src/main/java/edu/byuh/cis/cs203/hellocs203/GameActivity.java
@@ -0,0 +1,13 @@
+package edu.byuh.cis.cs203.hellocs203;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GameView vi = new GameView(this);
+        setContentView(vi);
+    }
+}
